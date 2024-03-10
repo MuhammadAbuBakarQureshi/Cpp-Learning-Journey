e8c21e8295740834b33754a7fabb6f7efedb9269
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-03-10 13:27:50 +0500

    New File

---
+++ Array/Exercise(W3-Resources)/Second-Largest-Number.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+void secondLargest(int arrSize, int arr[])
+{
+
+    int first = 0, second = 0;
+
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (arr[i] > first)
+        {
+
+            second = first;
+            first = arr[i];
+        }
+        else if (arr[i] > second)
+        {
+
+            second = arr[i];
+        }
+    }
+
+    cout << "Second Largest Number In The Array : " << second << endl;
+}
+
+int main()
+{
+
+    int n;
+
+    cout << "Input The Size Of Array : ";
+
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cout << endl;
+
+        cout << "Input The Value Of " << i << " Index : ";
+
+        cin >> arr[i];
+    }
+
+    secondLargest(n, arr);
+
+    return 0;
+}
