1d052d961f74dc5480a65773d00d50b50fb47d35
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-04-01 16:41:31 +0500

    Rectangle_Area.cpp added

---
+++ OOPs/Classes/Problems/Rectangle_Area.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Area
+{
+private:
+    float length;
+    float width;
+
+public:
+    Area(float l, float w)
+    {
+
+        length = l;
+        width = w;
+    }
+
+    int returnArea()
+    {
+
+        float result = length * width;
+        return result;
+    }
+};
+
+int main()
+{
+
+    int length, width;
+
+    cout << "Enter length of the rectangle : ";
+
+    cin >> length;
+
+    cout << "Enter width of the rectangle : ";
+
+    cin >> width;
+
+    Area a(length, width);
+
+    cout << "Area of Reactangle is : " << a.returnArea() << endl;
+
+    return 0;
+}
