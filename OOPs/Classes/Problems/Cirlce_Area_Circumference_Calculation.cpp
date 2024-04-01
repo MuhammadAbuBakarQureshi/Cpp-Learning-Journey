01f97a24115ca50c8a19cb0c3851c7499c197641
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-04-01 15:54:17 +0500

    Problems folder addded

---
+++ OOPs/Classes/Problems/Cirlce_Area_Circumference_Calculation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+const float pi = 3.14;
+
+class Circle
+{
+private:
+    float radius;
+
+public:
+    void setRadius(float r)
+    {
+
+        radius = r;
+    }
+
+    void CirlceArea()
+    {
+
+        float result = pi * (radius * radius);
+
+        cout << "Circle's area is : " << result << endl;
+    }
+
+    void CircleCircumference()
+    {
+
+        float result = 2 * pi * radius;
+
+        cout << "Circle's Circumference is : " << result << endl;
+    }
+};
+
+int main()
+{
+
+    Circle circle;
+
+    circle.setRadius(10);
+
+    circle.CirlceArea();
+
+    circle.CircleCircumference();
+
+    return 0;
+}
