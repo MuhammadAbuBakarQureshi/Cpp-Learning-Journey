dcc644a856711880813d34bc4137fa2bf0bb5251
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-04-20 13:50:15 +0500

    Concepts and Problems folder added

---
+++ Pointers And References/Problems/Level-1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // Q-NO-1 //Write a program to print the address of a variable whose value is input from user.
+
+    /*    int num;
+
+        cout << "Enter number : ";
+
+        cin >> num;
+
+        cout << "Address of the number you have entered : " << &num << endl;
+    */
+
+    // Q-NO-2 //Write a program to print the address of the pointer to a variable whose value is input from user.
+
+    /*
+        int num;
+
+        cout << "Enter number : ";
+
+        cin >> num;
+
+        int *ptr = &num;
+
+        cout << "Address of the number you have entered : " << ptr << endl;
+    */
+
+    // Q-NO-3 //Write a program to print the value of the address of the pointer to a variable whose value is input from user.
+
+    /*
+        int num, *ptr;
+
+        cout << "Enter number : ";
+
+        cin >> num;
+
+        ptr = &num;
+
+        cout << "Address of the pointer of " << num << " is " << &ptr << endl;
+    */
+
+    // Q-NO-4 //Write a program to print a number which is entered from keyboard using pointer.
+
+    /*
+        int num, *ptr;
+
+        cout << "Enter number : ";
+
+        cin >> num;
+
+        ptr = &num;
+
+        cout << *ptr << endl; // printing the value of num using pointer by derefrence
+    */
+    return 0;
+}
