9fb08c0e3b2bb806ec295c003e97a4370986758d
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-04-20 11:51:59 +0500

    Changes in increment.cpp

--- Pointers And References/Increment.cpp
+++ Pointers And References/Increment.cpp
@@ -7,6 +7,8 @@ int main()
 
     int num = 10;
 
+    cout << "Before increment: " << num << endl;
+
     int *ptr = &num; // address
 
     for (int i = 0; i < 5; i++){
@@ -14,7 +16,7 @@ int main()
         (*ptr)++; // increment after drefrence
     }
 
-    cout << num;
+    cout << "After increment: " << num << endl;
 
     return 0;
 }
