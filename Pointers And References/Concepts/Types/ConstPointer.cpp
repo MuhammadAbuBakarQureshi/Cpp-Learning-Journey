7716eb6c68f30a12f3802dd05fdbf1654b1fa58b
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-04-21 17:45:21 +0500

    Types folder added

---
+++ Pointers And References/Concepts/Types/ConstPointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int value = 10;
+
+    int *ptr = &value;
+
+    const int *ptrToConst = &value; // pointer to const
+
+    int *const constptr = &value; // constant pointer
+
+    // *ptrToConst = 20;       Error : cannot modify const data
+    *constptr = 20; 
+
+    cout << *constptr << endl;
+
+    int newValue = 20;
+
+    // constptr = &newValue; Error: cannot change the address of constant pointer
+
+    return 0;
+}
